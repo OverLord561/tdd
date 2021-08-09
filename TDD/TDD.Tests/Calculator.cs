f283fe00ec30bd6e10bd2fc0889aa9d8c1b9cc5b
overlord561 <yurapuk452@gmail.com>
2021-08-10 00:21:53 +0300

    6 tasks

--- TDD/TDD.Tests/Calculator.cs
+++ TDD/TDD.Tests/Calculator.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using System;
 using TDD;
 
 namespace Tests
@@ -10,14 +11,35 @@ namespace Tests
         {
         }
 
+
+
         [TestCase("", 0)]
         [TestCase("1", 1)]
         [TestCase("1,2", 3)]
-        public void Task1(string input, int expectedResult)
+        [TestCase("1,2,2", 5)]
+        [TestCase("1\n2,3", 6)]
+        [TestCase("1\n2,,\n3", 6)]
+        [TestCase("//;\n1;2", 3)]
+        [TestCase("//--\n1--2", 3)]
+        [TestCase("//--\n1\n2", 3)]
+
+        public void Task4(string input, int expectedResult)
         {
             int actualResult = Calculator.Task1(input);
 
             Assert.AreEqual(actualResult, expectedResult);
+        }
+
+        [TestCase("//--\n1\n-2", "negatives not allowed = -2")]
+        [TestCase("1,2,-3,-5,5,-7\n-8", "negatives not allowed = -3;-5;-7;-8")]
+        [TestCase("//;\n1;2;-3;-5;5;-7\n-8", "negatives not allowed = -3;-5;-7;-8")]
+
+
+        public void Task5(string input, string expectedResult)
+        {
+            Exception ex = Assert.Throws<Exception>(() => Calculator.Task1(input));
+
+            Assert.AreEqual(expectedResult, ex.Message);
         }
     }
 }
--- TDD/TDD/Calculator.cs
+++ TDD/TDD/Calculator.cs
@@ -1,18 +1,41 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace TDD
 {
     public static class Calculator
     {
+
+        ////;\n1;2
         public static int Task1(string numbers)
         {
+
             if (string.IsNullOrEmpty(numbers))
             {
                 return 0;
             }
 
-            var result = numbers.Split(',').Select(Int32.Parse).Sum();
+            var baseSeparatingStrings = new List<string>() { ",", "\n" };
+
+
+            if (numbers.StartsWith("//"))
+            {
+                var delimiter = string.Join("", numbers.Skip(2).Take(numbers.IndexOf("\n") - 2));
+                numbers = string.Join("", numbers.Skip(2 + delimiter.Length));
+
+                baseSeparatingStrings.Add($"{delimiter}");
+            }
+
+            
+            var integers = numbers.Split(baseSeparatingStrings.ToArray(), StringSplitOptions.RemoveEmptyEntries).Select(Int32.Parse);
+
+            var negative = integers.Where(x => x < 0);
+            if (negative.Any()) {
+                throw new Exception($"negatives not allowed = {string.Join(";", negative.ToArray())}");
+            }
+
+            var result = integers.Sum();
 
             return result;
         }
